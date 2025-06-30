cbcc69e28ef6a83440837a8f762e7da0c7d3b39f
Paxotio <36666921+Paxotio@users.noreply.github.com>
2025-06-30 13:36:06 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Cambiar el título al hacer clic
+document.querySelector("h1").addEventListener("click", () => {
+    alert("¡Hiciste clic en el título!");
+});
+
+// Opcional: Reloj en consola
+console.log("Página cargada a las:", new Date().toLocaleTimeString());
